8dc9b0d3dc035113924716c1053967472136cdd0
AsminaSheha <asminashehzadha@gmail.com>
2023-10-18 20:03:01 +0400

    Exercise 3.4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.4.py
@@ -0,0 +1,15 @@
+#guest list
+
+guests = ['Afthab', 'Jahangeer', 'Ajishad' , 'Shahitha']
+
+name = guests[0].title()
+print(f"{name}, It's time you come eat my cooking.Expecting you this Saturday.")
+
+name = guests[1].title()
+print(f"{name}, It's time you come eat my cooking.Expecting you this Saturday.")
+
+name = guests[2].title()
+print(f"{name}, It's time you come eat my cooking.Expecting you this Saturday.")
+
+name = guests[3].title()
+print(f"{name}, It's time you come eat my cooking.Expecting you this Saturday.")
