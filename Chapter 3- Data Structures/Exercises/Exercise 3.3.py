2b860c049109b4bcb5c035c2d3e55decf5d9fec3
AsminaSheha <asminashehzadha@gmail.com>
2023-10-18 12:16:16 +0400

    Exercise 3.3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.3.py
@@ -0,0 +1,8 @@
+#Favorite transport
+
+cars = [" Ferrari "," Mercedes "," Honda "]
+transport =[" Bus "," cycle "," Metro "]
+
+print ("my favorite type of transport is by" + transport[1])
+print ("But i can only afford a" + transport[0] + "and" + transport[2])
+print ("I would like to own a" + cars[0] + "," + cars[1] + "or atleast a " + cars[2])
