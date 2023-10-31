728486f3cc2c722a31444b1808021ee0d2e50914
AsminaSheha <asminashehzadha@gmail.com>
2023-10-31 09:33:38 +0400

    Exercise 4.3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 4.3.py
@@ -0,0 +1,31 @@
+#Turn your if-else chain from Exercise 5-4 into an if-elifelse chain.
+
+#alien is yellow
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points.")
+else:
+    print("You just earned 15 points.")
+
+#alien is red
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points.")
+else:
+    print("You just earned 15 points.")
+
+#alien is green
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points.")
+else:
+    print("You just earned 15 points.")
