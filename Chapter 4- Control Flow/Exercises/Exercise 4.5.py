68bd7e3ca288e29fcc83ed99984af80581d2abdd
AsminaSheha <asminashehzadha@gmail.com>
2023-10-31 09:47:29 +0400

    Exercise 4.5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 4.5.py
@@ -0,0 +1,19 @@
+'''Make a list of your favorite fruits,
+and then write a series of independent if statements that check for certain fruits in your list.'''
+
+favorite_fruits = ["Dragon Fruit", "Rambutan", "Mangosteen"]
+
+if "Mangosteen" in favorite_fruits:
+    print("You really like Mangosteen!")
+
+if "Durian" in favorite_fruits:
+    print("You really like Durian!")
+
+if "Rambutan" in favorite_fruits:
+    print("You really like Rambutan!")
+
+if "Monstera" in favorite_fruits:
+    print("You really like Monstera!")
+
+if "Jackfruit" in favorite_fruits:
+    print("You really like Jackfruit!")
