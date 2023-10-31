fd2a861124c96475a7db231c1a63bd45fe0982bb
AsminaSheha <asminashehzadha@gmail.com>
2023-10-31 09:30:14 +0400

    Exercise 4.2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 4.2.py
@@ -0,0 +1,18 @@
+#Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain
+
+#if block
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print ("you just earned 5 points")
+else:
+    print ("you just earned 10 points")
+
+#else block
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print ("you just earned 5 points")
+else:
+    print ("you just earned 10 points")
