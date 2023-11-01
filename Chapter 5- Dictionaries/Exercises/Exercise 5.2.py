58b6aaf57f4e73bc381ed142eca2998ec786acbe
AsminaSheha <asminashehzadha@gmail.com>
2023-11-01 22:09:22 +0400

    Exercise 5.2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 5.2.py
@@ -0,0 +1,24 @@
+#glossary with what I've learned
+
+glossary = {
+    "Variable": "A named storage location in a program to store data.",
+    "Function": "A block of reusable code that performs a specific task.",
+    "List": "An ordered collection of items that can be of different data types.",
+    "Loop": "A control structure that repeats a set of instructions until a condition is met.",
+    "Dictionary": "A collection of key-value pairs for efficient data retrieval.",
+    }
+
+word = 'Variable'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'Function'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'List'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'Loop'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'Dictionary'
+print("\n" + word.title() + ": " + glossary[word])
