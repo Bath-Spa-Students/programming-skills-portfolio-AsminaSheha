9ab12546679a8c017386159a32527ed2bb8bb20c
AsminaSheha <asminashehzadha@gmail.com>
2023-11-01 10:42:26 +0400

    Exercise 5.1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 5.1.py
@@ -0,0 +1,15 @@
+#Using dictionary to store the information of Hajara Ishaq
+
+person = {
+    'first_name': 'Hajara',
+    'last_name': 'Ishaq',
+    'age': 19,
+    'city': 'Sharjah',
+    }
+
+#print the information
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
