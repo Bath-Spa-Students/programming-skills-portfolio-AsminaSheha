289fdeb4604a19a289ec985c4628935434029516
AsminaSheha <asminashehzadha@gmail.com>
2023-11-08 09:51:07 +0400

    Practice 2.5.py

---
+++ Chapter 2- Variables & Comments/Practices/Practice 2.5.py
@@ -0,0 +1,10 @@
+# Store fruit names in a list
+fruits = ["apple Mango", "Rambutan", "Dragon Fruit"]
+
+# Unpack the list into three variables
+x, y, z, *remaining = fruits
+
+# Print the values of each variable
+print("x:", x)
+print("y:", y)
+print("z:", z)
