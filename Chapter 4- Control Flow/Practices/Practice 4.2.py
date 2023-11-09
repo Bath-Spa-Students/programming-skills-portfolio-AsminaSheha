c6e383b742e22ee2b4daa0a5610d4e73b24d653d
AsminaSheha <asminashehzadha@gmail.com>
2023-11-09 23:36:27 +0400

    Practice 4.2.py

---
+++ Chapter 4- Control Flow/Practices/Practice 4.2.py
@@ -0,0 +1,13 @@
+# Assume variable a is given a value
+a = 20
+
+# Check if the variable a is less than 10
+if a < 10:
+    # If true, assign 'O' to variable b
+    b = 'O'
+else:
+    # If false, assign 99 to variable b
+    b = 99
+
+# Print the value of variable b
+print("Value of b:", b)
