65d168a4858d2d71a44d044453cfce82eeeed7e9
AsminaSheha <asminashehzadha@gmail.com>
2023-11-11 17:13:27 +0400

    Practice 5.2.py

---
+++ Chapter 5- Dictionaries/Practices/Practice 5.2.py
@@ -0,0 +1,14 @@
+#Write a Python program to iterate through the keys of a dictionary and print them.
+
+# Sample dictionary
+my_dictionary = {
+    "name": "Asmina Shehzadha",
+    "age": 19,
+    "city": "Dubai",
+    "occupation": "Student"
+}
+
+# Iterate through the keys and print them
+print("Keys in the dictionary:")
+for key in my_dictionary.keys():
+    print(key)
