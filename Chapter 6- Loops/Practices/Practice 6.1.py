689870e9f82cc6c905306be2469adb38c9c9b83f
AsminaSheha <asminashehzadha@gmail.com>
2023-11-11 17:37:48 +0400

    Practice 6.1.py

---
+++ Chapter 6- Loops/Practices/Practice 6.1.py
@@ -0,0 +1,10 @@
+#Write a Python program that uses a while loop to print the even numbers from 2 to 10
+
+# Initialize a variable with the starting value
+number = 2
+
+# Use a while loop to print even numbers from 2 to 10
+while number <= 10:
+    print(number)
+# Increment by 2 to get the next even number
+    number += 2  
