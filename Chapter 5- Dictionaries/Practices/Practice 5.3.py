f016d36a6cb8eb42de4321cb5ba8f08a0d683f50
AsminaSheha <asminashehzadha@gmail.com>
2023-11-11 17:16:26 +0400

    Practice 5.3.py

---
+++ Chapter 5- Dictionaries/Practices/Practice 5.3.py
@@ -0,0 +1,14 @@
+#Write a Python program to iterate through the values of a dictionary and print them.
+
+# Sample dictionary
+my_dictionary = {
+    "name": "Asmina Shehzadha",
+    "age": 19,
+    "city": "Dubai",
+    "occupation": "Student"
+}
+
+# Iterate through the values and print them
+print("Values in the dictionary:")
+for value in my_dictionary.values():
+    print(value)
